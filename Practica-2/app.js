a623a397a0d7353d6d86637f10fb8e1f0fd75ce1
Osel Nicolás <oselko123@gmail.com>
2026-04-28 15:55:50 +0200

    Probando swagger

--- Practica-2/app.js
+++ Practica-2/app.js
@@ -6,6 +6,34 @@ import products from './routes/products.js';
 
 app.use(express.json());
 
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUI from 'swagger-ui-express';
+const swaggerOptions = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'ACME Technologies',
+            version: '1.0.0',
+            description: 'App test APIRest',
+            contact: {
+                name: 'John Doe',
+                email: 'john@doe.com'
+            }
+        },
+        servers: [
+            {url: 'http://localhost:3000'}
+        ],
+        supportedSubmitMethods: []
+    },
+    apis: [
+        './routes/products.js'
+    ]
+}
+
+const swaggerSpecs = swaggerJSDoc(swaggerOptions);
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpecs, {customCss: '.swagger-ui .topbar, .swagger-ui .try-out {display: none}'}))
+
 app.use('/products', products);
 app.use((req, res) => {
     res.status(404).json({
