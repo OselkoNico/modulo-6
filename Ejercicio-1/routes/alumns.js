835ec08bbffb6de8edbdc8eafce4ad416be590ff
Osel Nicolás <oselko123@gmail.com>
2026-04-29 11:45:38 +0200

    Crear archivo alumns.js y método get

---
+++ Ejercicio-1/routes/alumns.js
@@ -0,0 +1,24 @@
+import express from 'express';
+const router = express.Router();
+
+const alumns = [
+    { name: "Juan", surname: "Gomez Alvarez", age: "23" },
+    { name: "Ana", surname: "Martin Chal", age: "20" },
+    { name: "Antonio", surname: "Benitez Royal", age: "21" }
+]
+
+router.get('/alumnos', (req, res) => {
+    if (!req.query.name) {
+        return res.status(400).json({
+            message: 'Incorrect query params'
+        })
+    }
+    const devolverAlumnos = alumns.filter(elem => elem.name === req.query.name);
+    console.log(devolverAlumnos)
+    res.status(200).json({
+        message: 'Ok',
+        alumnos: devolverAlumnos
+    })
+})
+
+export default router;
